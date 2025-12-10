17b1aeb0d73c48536b908fc238994c7a033f4c04
Yash09102003 <163733841+Yash09102003@users.noreply.github.com>
2025-12-10 06:51:08 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 45752
+let accountEmail = "yash112@yahho.com"
+var accountPassword = "11111"
+accountCity = "Pilibangan"
+
+// do not ever use var due to block scope
+
+// accountId = 2 not valid
+
+accountEmail = "hello6969@google.com"
+accountPassword = "99999"
+accountCity = "Jaipur"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity])
+
+
+
