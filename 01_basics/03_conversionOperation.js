6c8c6f53d6d5fac5d81e3e06c5fe7dcee53f4579
Yash09102003 <163733841+Yash09102003@users.noreply.github.com>
2025-12-10 07:22:51 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+let score = "44xyz"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)  //converts into number
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = "hello"
+
+let booleanisLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "hello" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof (stringNumber))
+
+// **************************Operations************************************
+
+let value = 4
+let negValue = -value
+// console.log(negValue)
+
+// console.log(3+4)
+// console.log(3-4)
+// console.log(3*4)
+// console.log(3**4)
+// console.log(3/4)
+// console.log(3%4)
+
+let str1 = "yash"
+let str2 = " bansal"
+
+let str3 = str1 + str2 
+// console.log(str3)
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+
