6ad10c6156348f9a291905f24f79e0021a54b8e3
Yash09102003 <163733841+Yash09102003@users.noreply.github.com>
2025-12-10 07:14:56 +0000

    Added JS datatypes example

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) // we are using node.js, not browser
+
+console.log( 3 + 3);
+
+console.log("Yash")
+
+let name = "Yash"
+let age = 22
+let isLoggedIn = false
+
+// number= 2^53
+//bigint in case of trading website etc
+ // string => ""
+ // boolean = true/false
+ // null => standalone value
+//  undefined => valuve not defined
+// symbol => unique
+// object
+
+console.log(typeof undefined)
+// null is a object while undefined is undefined
